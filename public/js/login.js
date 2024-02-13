97c34584b1fcaffdec086439e97deca57018facc
Zion Griffin <defendingmymaininbox@gmail.com>
2024-02-12 22:49:32 -0500

    Login and signup pages are functional

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,22 @@
 const loginHandler = async(e) => {
     e.preventDefault();
 
-    
-}
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#pass').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST', 
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in');
+        }
+    }
+};
+
+document.querySelector('#login-form').addEventListener('submit', loginHandler);
